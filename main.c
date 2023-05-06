d7731227b9563228d12d6a87aa4ff4e325bfa8d9
Cam <cjryanwashere@gmail.com>
2023-05-06 19:00:26 -0400

    added feedforward node

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+//#include <omp.h>
 
 typedef struct 
 {
@@ -47,6 +48,29 @@ tensor * tensor_copy(tensor * t) {
     }
 
     return new_t;
+}
+/**
+ * @brief Tensor Broadcast Add
+ * 
+ * Add the elements of t1 to t0, elementwise.
+ * 
+ * after the function: 
+ * 
+ * t0 = t0 + t1
+ * t1 = t1
+ * 
+ * @param t0 
+ * the tensor being added to 
+ * @param t1 
+ * the tensor being added to t0
+ */
+void tensor_broadcast_add(tensor * t0, tensor * t1)
+{
+    int num_elems = tensor_num_elems(t0);
+
+    for (int i = 0; i < num_elems; i ++) {
+        t0->data[i] += t1->data[i];
+    }
 }
 void free_tensor(tensor * t) 
 {
@@ -101,12 +125,60 @@ void tensor_print(tensor * t) {
 }
 
 
+int tensor_vector_transformation(tensor * v, tensor * m, tensor * out)
+{
+    // make sure that the tensors are valid
+    if (v->rank != 1) { fprintf(stderr, "tensor_vector_transformation: v should be of rank 1"); return 1; }
+    if (m->rank != 2) { fprintf(stderr, "tensor_vector_transformation: m should be of rank 2"); return 1; }
+    if (v->shape[0] != m->shape[1]) { fprintf(stderr, "tensor_vector_transformation: v and m have incompatible shapes"); return 1; }
+
+    int out_dim = out->shape[0];
+
+    // for each row in the matrix
+    for (int i = 0; i < m->shape[0]; i++) {
+        float dot_product = 0.0f;
+        int row_idx_offset = i * out_dim;
+        // for each column in the matrix, and element of out
+        for (int j = 0; j < out_dim; j++) {
+            dot_product += out->data[j] * m->data[row_idx_offset + j];
+        }
+        out->data[i] = dot_product;
+    }
+
+    return 0;
+}
 
 
 typedef struct 
-{
+{   
+    int hidden_dim;
+
+    tensor * w_1;
+    tensor * b_1;
+
+    tensor * w_2;
+    tensor * b_2;
+
+    tensor * hidden_cache;
+    tensor * output_cache;
 
 } feedforward_node;
+feedforward_node * feedforward_node_create(int dim, int hidden_dim) 
+{
+    feedforward_node * ff = (feedforward_node*) malloc(sizeof(feedforward_node));
+
+    int w_1_shape[2] = { hidden_dim, dim };
+    ff->w_1 = tensor_create(2, w_1_shape);
+    int b_1_shape[1] = { hidden_dim };
+    ff->b_1 = tensor_create(1, b_1_shape);
+
+    int w_2_shape[2] = { dim, hidden_dim };
+    ff->w_2 = tensor_create(2, w_2_shape);
+    int b_2_shape[1] = { dim };
+    ff->b_2 = tensor_create(1, b_2_shape);
+
+    return ff;
+}
 
 /**
  * @brief Attention Node
@@ -115,21 +187,20 @@ typedef struct
  **/
 typedef struct 
 {
-
-    // for full clarity, these are NOT matrices!
-    // these is just the weights for one particular node,
-    // and therefore they are just one dimensional vectors.
     tensor * w_key;
+    tensor * b_key;
+
     tensor * w_query;
+    tensor * b_query;
+
+    tensor * w_value;
     tensor * w_value; 
     
-
+    // the dimensionality of all the weight vectors
+    int rank;
 
     tensor * attention_weight_matrix;
     tensor * output_cache;
-
-    // the dimensionality of all the weight vectors
-    int rank;
 
 } attention_node; 
 
@@ -146,7 +217,6 @@ typedef struct
     feedforward_node * feedforward;
 
     tensor * output_cache;
-
 } block_node;
 
 typedef struct 
@@ -327,13 +397,47 @@ void attention_node_forward(attention_node * node, int node_idx, int head_c, blo
     tensor_print(node->output_cache);
 }
 
+/**
+ * @brief Tensor Zeros
+ * 
+ * sets every element of t to 0
+ * 
+ * @param t 
+ * the tensor being turned set to zeros
+ */
+void tensor_zeros(tensor * t) 
+{
+    int num_elems = tensor_num_elems(t);
+
+    //#pragma omp parallel for 
+    for (int i = 0; i < num_elems; i++) {
+        t->data[i] = 0;
+    }
+}
 
 int main ()
 {
     int shape0[1] = {16};
 
     tensor * t0 = tensor_create(1, shape0);
-    for (int i = 0; i < t0->shape[0]; i++) {t0->data[i] = 1.5;}
+    tensor_zeros(t0);
+
+    tensor_print(t0);
+
+
+    
+
+   
+
+    return 0;
+}
+
+int test1()
+{
+    int shape0[1] = {16};
+
+    tensor * t0 = tensor_create(1, shape0);
+    for (int i = 0; i < t0->shape[0]; i++) {t0->data[i] = 1.5;};
 
     tensor * t1 = tensor_create(1, shape0);
     for (int i = 0; i < t1->shape[0]; i++) {t1->data[i] = 1.0;}
@@ -348,10 +452,8 @@ int main ()
     attention_node * node = (attention_node*) malloc(sizeof(attention_node));
 
     attention_node_forward(node, 0, 4, input_blocks, 2);
-
-
     return 0;
-}
+} 
 
 
 int test0() 
@@ -374,5 +476,5 @@ int test0()
     t3->data = out;
     tensor_print(t3);
 
-    return 0;
+   // return 0;
 }
