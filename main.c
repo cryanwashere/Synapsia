6dbf39ff585b6e1e91a383419902e441d8e32243
Cameron Ryan <cjryanwashere@gmail.com>
2023-05-06 11:58:23 -0400

    pushing minor changes

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+typedef struct {
+
+} transformer_node;
+
+typedef struct {
+
+    // the number of layers that the transformer has
+    int num_layers;
+
+
+}   transformer;
+
+
+
+int main ()
+{
+    return 0;
+}
